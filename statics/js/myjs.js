bb0fbbdae9db7f2930de51fbe7fcb4f6f151e772
cszeus <MinweiShen1991@gmail.com>
2014-05-11 16:56:31 +0800

    version 1.0
    基本功能完成，可手动编辑已看剧集。

--- statics/js/myjs.js
+++ statics/js/myjs.js
@@ -1,4 +1,6 @@
 document.getElementById("add").addEventListener("click",addNewItem);
+document.getElementById("showAddItem").addEventListener("click",setSelectDay);
+document.getElementById("searchBtn").addEventListener("click",search);
 
 $(document).ready(function() {
 	var d = new Date().getDay();
@@ -7,17 +9,14 @@ $(document).ready(function() {
 
     var data = window.localStorage.getItem('record');
     var record = JSON.parse(data);
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     if(record == null){
-    	$("#Monday").html("今天没什么好看的诶~");
-    	$("#Tuesday").html("今天没什么好看的诶~");
-    	$("#Wednesday").html("今天没什么好看的诶~");
-    	$("#Thursday").html("今天没什么好看的诶~");
-    	$("#Friday").html("今天没什么好看的诶~");
-    	$("#Saturday").html("今天没什么好看的诶~");
-    	$("#Sunday").html("今天没什么好看的诶~");
+        for(i in days){
+            var id = "#"+days[i];
+            $(id).html("今天没什么好看的诶~");
+        }
     }
     else{
-    	var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     	for(i in days){
     		var day = days[i];
     		var dayRecord = record[day];
@@ -100,94 +99,209 @@ $(document).ready(function() {
     }
 });
 
+
+function search(){
+    var keyword = $("#searchText").val().toLowerCase();
+    var record = JSON.parse(window.localStorage.getItem('record'));
+    var found = false;
+    var foundDay = null;
+    var foundName = null;
+    for(day in record){
+        if(!found){
+            var dayRecord = record[day];
+            for(name in dayRecord){
+                if(name.toLowerCase().indexOf(keyword) != -1){
+                    found = true;
+                    foundName = name;
+                    foundDay  = day;
+                }
+            }
+        }
+    }
+    if(found){
+        var href = "#"+foundDay;
+        var whichTab = "#dayTab a[href='"+href+"']";
+        $(whichTab).tab('show');
+    }
+}
+
+function setSelectDay(){
+    var d = new Date().getDay();
+    $('#daySelect option:eq('+d+')').prop('selected', true);
+}
+
 function addNewItem(){
 	var day = $("#daySelect").val();
 	var name = $("#aniName").val();
-	var record = window.localStorage.getItem('record');
-	if(record == null){
-		//no record that day,create a new one
-		var jsonData = '{"' + day + '":{"'+name+'":1}}';
-		window.localStorage.setItem('record',jsonData);
-	}
-	else{
-		console.log(record);
-		var obj = JSON.parse(record);
-		var dayRecord = obj[day];
-		if(dayRecord == null){
-			//no record for that day
-			var s = {};
-			s[name] = 1;
-			obj[day] = s;
-		}
-		else{
-			var aniRecord = dayRecord[name];
-			if(aniRecord == null){
-				//no record in that day for that animation
-				dayRecord[name] = 1;
-				obj[day] = dayRecord;
-			}
-		}
-		jsonData = JSON.stringify(obj);
-		window.localStorage.setItem('record',jsonData);
-	}
-	window.location.reload();
+	if(name != ''){
+        var record = window.localStorage.getItem('record');
+        if(record == null){
+            //no record that day,create a new one
+            var jsonData = '{"' + day + '":{"'+name+'":1}}';
+            window.localStorage.setItem('record',jsonData);
+        }
+        else{
+            console.log(record);
+            var obj = JSON.parse(record);
+            var dayRecord = obj[day];
+            if(dayRecord == null){
+                //no record for that day
+                var s = {};
+                s[name] = 1;
+                obj[day] = s;
+            }
+            else{
+                var aniRecord = dayRecord[name];
+                if(aniRecord == null){
+                    //no record in that day for that animation
+                    dayRecord[name] = 1;
+                    obj[day] = dayRecord;
+                }
+            }
+            jsonData = JSON.stringify(obj);
+            window.localStorage.setItem('record',jsonData);
+        }
+        window.location.reload();
+    }
+    else{
+        $("#newitem").collapse("hide");
+    }
+	
 }
 
 function plusAni(target){
-    var title = target.title;
-    var record = JSON.parse(window.localStorage.getItem('record'));
-    var found = false;
-    for(day in record){
-        if(!found){
-            var dayRecord = record[day];
-            for (name in dayRecord){
-                if(name == title){           
-                    var num = dayRecord[name];
-                    num += 1;
-                    dayRecord[name] = num;
-                    found = true;
+    var episodeNode = target.parentNode.parentNode.childNodes[1];
+    if(episodeNode.childNodes[0].nodeType == 3){
+        //if in the edit mode, the child is an input and the return value is 1
+        var title = target.title;
+        var record = JSON.parse(window.localStorage.getItem('record'));
+        var found = false;
+        for(day in record){
+            if(!found){
+                var dayRecord = record[day];
+                for (name in dayRecord){
+                    if(name == title){           
+                        var num = dayRecord[name];
+                        num += 1;
+                        dayRecord[name] = num;
+                        found = true;
+                    }
                 }
             }
         }
+        window.localStorage.setItem('record',JSON.stringify(record));
+        var num = parseInt(episodeNode.innerHTML) + 1;
+        episodeNode.innerHTML = num;
     }
-    window.localStorage.setItem('record',JSON.stringify(record));
-    var episodeNode = target.parentNode.parentNode.childNodes[1];
-    var num = parseInt(episodeNode.innerHTML) + 1;
-    episodeNode.innerHTML = num;
 }
 
 function minusAni(target){
+    var episodeNode = target.parentNode.parentNode.childNodes[1];
+    if(episodeNode.childNodes[0].nodeType == 3){
+        //if in the edit mode, the child is an input and the return value is 1
+        var title = target.title;
+        var title = target.title;
+        var record = JSON.parse(window.localStorage.getItem('record'));
+        var found = false;
+        for(day in record){
+            if(!found){
+                var dayRecord = record[day];
+                for (name in dayRecord){
+                    if(name == title){           
+                        var num = dayRecord[name];
+                        if(num > 0){
+                            num = num - 1;
+                        }
+                        dayRecord[name] = num;
+                        found = true;
+                    }
+                }
+            }
+        }
+        window.localStorage.setItem('record',JSON.stringify(record));
+        
+        var num = parseInt(episodeNode.innerHTML);
+        if(num > 0){
+            num -= 1;
+        }
+        episodeNode.innerHTML = num;
+    }
+}
+
+function editAni(target){
+    var episodeNode = target.parentNode.parentNode.childNodes[1];
+    if(episodeNode.childNodes[0].nodeType == 3){
+        var text = document.createElement("input")
+        text.type = "text";
+        text.style.width = "30px";
+        text.style.height = "19px";
+        text.style.padding = "0 0 0 0";
+        text.style.textAlign = "right";
+        text.style.fontSize = "11px";
+        var num = parseInt(episodeNode.innerHTML);
+        text.value = num;
+        episodeNode.innerHTML = '';
+        episodeNode.appendChild(text);
+    }
+    else{
+        var textNode = episodeNode.childNodes[0]
+        var num = parseInt(textNode.value);
+        var title = target.title;
+        var record = JSON.parse(window.localStorage.getItem('record'));
+        var found = false;
+        for(day in record){
+            if(!found){
+                var dayRecord = record[day];
+                for (name in dayRecord){
+                    if(name == title){           
+                        dayRecord[name] = num;
+                        found = true;
+                    }
+                }
+            }
+        }
+        window.localStorage.setItem('record',JSON.stringify(record));
+        episodeNode.removeChild(textNode)
+        episodeNode.innerHTML = num;
+    }
+    
+}
+
+function deleteAni(target){
     var title = target.title;
     var record = JSON.parse(window.localStorage.getItem('record'));
     var found = false;
+    var daytodelete = null;
     for(day in record){
         if(!found){
             var dayRecord = record[day];
             for (name in dayRecord){
                 if(name == title){           
-                    var num = dayRecord[name];
-                    if(num > 0){
-                        num = num - 1;
-                    }
-                    dayRecord[name] = num;
                     found = true;
+                }
+            }
+            if(found){
+                delete dayRecord[title];
+                record[day] = dayRecord;
+                var trNode = target.parentNode.parentNode;
+                var tbodyNode = trNode.parentNode;
+                var size = 0;
+                for (key in dayRecord){
+                    size += 1;
+                }
+                if(size == 0){
+                    daytodelete = day;
+                    var tableNode = tbodyNode.parentNode;
+                    var divNode = tableNode.parentNode;
+                    divNode.removeChild(tableNode);
+                    divNode.innerHTML =  "今天没什么好看的诶~";
+                }
+                else{
+                    tbodyNode.removeChild(trNode);
                 }
             }
         }
     }
+    delete record[daytodelete];
     window.localStorage.setItem('record',JSON.stringify(record));
-    var episodeNode = target.parentNode.parentNode.childNodes[1];
-    var num = parseInt(episodeNode.innerHTML);
-    if(num > 0){
-        num -= 1;
-    }
-    episodeNode.innerHTML = num;
-}
-
-function editAni(target){
-    console.log(title);
-}
-
-function deleteAni(target){
-    console.log(title);
 }
